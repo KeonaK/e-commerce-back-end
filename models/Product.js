9a81c3e14d00bd84f7aab70fa14c3eb1cea2b8e0
Keona Kirby <keonakirby@gmail.com>
2021-04-25 17:13:01 -0500

    Add/columns to product.js

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,69 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+  
+
+  // * `id`
+  id: {
+    //   * Integer.
+    type: DataTypes.INTEGER,
+    //   * Doesn't allow null values.
+    allowNull: false,
+    //   * Set as primary key.
+    primaryKey: true,
+    //   * Uses auto increment.
+    autoIncrement: true,
+  },
+
+  // * `product_name`
+  product_name: {
+    //   * String.
+    type: DataTypes.STRING,
+    //   * Doesn't allow null values.
+    allowNull: false,
+  },
+
+  // * `price`
+  price:{
+    //   * Decimal.
+    type: DataTypes.DECIMAL,
+    //   * Doesn't allow null values.
+    allowNull: false,
+    //   * Validates that the value is a decimal.
+    validate: {
+      isDecimal: true,
+    }
+
+  },
+
+  // * `stock`
+  stock: {
+    //   * Integer.
+    type: DataTypes.INTEGER,
+    //   * Doesn't allow null values.
+    allowNull: false,
+    //   * Set a default value of `10`.
+    defaultValue: 10,
+    //   * Validates that the value is numeric.
+    validate: {
+      isNumeric: true,
+    }
+  },
+
+  // * `category_id`
+  category_id:{
+    //   * Integer.
+    type: DataTypes.INTEGER,
+    //   * References the `Category` model's `id`.
+    references: {
+      model: "category",
+      key: "id",
+      unique: false,
+    }
+
+  }
+
+  
   },
   {
     sequelize,
